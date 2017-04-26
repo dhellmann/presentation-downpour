eecd4b8e88fd28a9ab75fb682034a2f4637b5ad2
Doug Hellmann <doug@doughellmann.com>
2017-04-26 18:29:59 -0400

    rsync the fonts directory

--- bin/rsync.sh
+++ bin/rsync.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-FILES="*.html css js lib plugin *.svg img"
+FILES="*.html css js lib plugin *.svg img fonts"
 
 ssh doughellmann.com 'mkdir -p ~/doughellmann.com/presentations/downpour-lightning-talk'
 
